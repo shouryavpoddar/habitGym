7bb6771aee20c496a58483d35a317782f9f0c88c
shouryavpoddar <135847765+shouryavpoddar@users.noreply.github.com>
2023-06-09 15:27:43 +0530

    Update DisplayPage.dart
    Cleaned the code

--- lib/DisplayPage.dart
+++ lib/DisplayPage.dart
@@ -14,18 +14,25 @@ class DisplayPage extends StatefulWidget{
 }
 
 class Page extends State<DisplayPage> {
+  // stores the id of the user to negate re-Login
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  final Completer<WebViewController> _controller = Completer<
-      WebViewController>();
 
-  late Future<String?> _HomeUrl;
+  // Controls the Webview
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
 
+  // stores the Home Url, will be null when there is no saved id
+  late Future<String?> _homeUrl;
+
+  // visibilty indicator for the pop up menu
   bool _visible = false;
 
+  /**
+   * run only once by the program to see if there exits a home url
+   */
   @override
   void initState() {
     super.initState();
-    _HomeUrl = _prefs.then((SharedPreferences prefs) {
+    _homeUrl = _prefs.then((SharedPreferences prefs) {
       String url = prefs.getString("homeUrl") ?? "https://www.thehabitgym.com/reflections/activate";
       setState(() {
         _visible = !(url == "https://www.thehabitgym.com/reflections/activate");
@@ -34,181 +41,123 @@ class Page extends State<DisplayPage> {
     });
   }
 
+  /**
+   * visual the class is building
+   */
   @override
   Widget build(BuildContext context) {
     return getScaffold();
   }
 
+  /**
+   * Saves the Home url
+   */
   void homePageSaver(String url) async {
     final SharedPreferences pref = await this._prefs;
     pref.setString("homeUrl", url);
   }
 
+  /**
+   * deletes teh Home url
+   */
   void homePageDelete() async {
     final SharedPreferences pref = await this._prefs;
     pref.remove("homeUrl");
   }
 
-  Widget getHamburgerMenu() {
+  /**
+   * getter for the PopupMenu and the Logout button in it
+   */
+  Widget getPopupMenu() {
     return FutureBuilder<WebViewController>(
-      future: _controller.future,
-      builder: (BuildContext build,
-          AsyncSnapshot<WebViewController> controller) {
-        if (controller.hasData) {
-          return Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  const DrawerHeader(
-                    decoration: BoxDecoration(
-                      /*image: const DecorationImage(
-                      image: Image.asset("assests/images/Logo.pnd")
-                    ),*/
+        future: _controller.future,
+        builder: (BuildContext build, AsyncSnapshot<WebViewController> controller){
+          return PopupMenuButton(
+              itemBuilder: (BuildContext context){
+                return [
+                  PopupMenuItem(
+                    child: TextButton(
+                      child: const Text("Log out"),
+                      onPressed: () {
+                        setState(() {
+                          controller.data?.loadUrl("https://www.thehabitgym.com/reflections/activate");
+                          _visible = false;
+                          homePageDelete();
+                        });
+                      },
                     ),
-                    child: Text('Drawer Header'),
-                  ),
-                  ListTile(
-                    title: const Text('Log Out'),
-                    onTap: () {
-                      setState(() {
-                        controller.data?.loadUrl(
-                            "https://www.thehabitgym.com/reflections/activate");
-                        _visible = false;
-                        homePageDelete();
-                      });
-                    },
-                  ),
-                ],
-              )
+                  )
+                ];
+              }
           );
         }
-        return Container();
-      },
     );
   }
+
+  /**
+   * getter for the Scaffold for the app
+   */
   Widget getScaffold() {
     if(_visible){
       return Scaffold(
-        body: FutureBuilder<String?>(
-          future: _HomeUrl,
-          builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return const CircularProgressIndicator();
-              case ConnectionState.active:
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return WebView(
-                    javascriptMode: JavascriptMode.unrestricted,
-                    initialUrl: snapshot.data,
-                    onWebViewCreated: (WebViewController webViewController) {
-                      _controller.complete(webViewController);
-                    },
-                    navigationDelegate: (NavigationRequest request) {
-                      if (request.url.startsWith(
-                          "https://www.thehabitgym.com/reflections/home/")) {
-                        homePageSaver(request.url);
-                      }
-                      if (request.url.startsWith(
-                          "https://www.thehabitgym.com/reflections/activate") ==
-                          false) {
-                        setState(() {
-                          _visible = true;
-                        });
-                      }
-                      return NavigationDecision.navigate;
-                    },
-                  );
-                }
-            }
-          },
-        ),
+        body: getWebView(),
         appBar: AppBar(
-          title: Text("Habit Gym"),
-          backgroundColor: Color(0xFFfa8221),
+          title: const Text("Habit Gym"),
+          backgroundColor: const Color(0xFFfa8221),
           actions: [
-            FutureBuilder<WebViewController>(
-                future: _controller.future,
-                builder: (BuildContext build, AsyncSnapshot<WebViewController> controller){
-                  return PopupMenuButton(
-                      itemBuilder: (BuildContext context){
-                        return [
-                          PopupMenuItem(
-                            child: Visibility(
-                              visible: _visible,
-                              child: TextButton(
-                                child: Text("Log out"),
-                                onPressed: () {
-                                  setState(() {
-                                    controller.data?.loadUrl("https://www.thehabitgym.com/reflections/activate");
-                                    _visible = false;
-                                    homePageDelete();
-                                  });
-                                },
-                              ),
-                            ),
-                          )
-                        ];
-                      }
-                  );
-                }
-            )
+            getPopupMenu(),
           ],
         ),
       );
     }
     else{
       return Scaffold(
-        body: FutureBuilder<String?>(
-          future: _HomeUrl,
-          builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return const CircularProgressIndicator();
-              case ConnectionState.active:
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return WebView(
-                    javascriptMode: JavascriptMode.unrestricted,
-                    initialUrl: snapshot.data,
-                    onWebViewCreated: (WebViewController webViewController) {
-                      _controller.complete(webViewController);
-                    },
-                    navigationDelegate: (NavigationRequest request) {
-                      if (request.url.startsWith(
-                          "https://www.thehabitgym.com/reflections/home/")) {
-                        homePageSaver(request.url);
-                      }
-                      if (request.url.startsWith(
-                          "https://www.thehabitgym.com/reflections/activate") ==
-                          false) {
-                        setState(() {
-                          _visible = true;
-                        });
-                      }
-                      return NavigationDecision.navigate;
-                    },
-                  );
-                }
-            }
-          },
-        ),
+        body: getWebView(),
         appBar: AppBar(
-          title: Text("Habit Gym"),
-          backgroundColor: Color(0xFFfa8221),
+          title: const Text("Habit Gym"),
+          backgroundColor: const Color(0xFFfa8221),
         ),
       );
-    };
+    }
   }
 
+  /**
+   * getter for the WebView for the user
+   */
+  Widget getWebView(){
+    return FutureBuilder<String?>(
+      future: _homeUrl,
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return const CircularProgressIndicator();
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return WebView(
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: snapshot.data,
+                onWebViewCreated: (WebViewController webViewController) {
+                  _controller.complete(webViewController);
+                },
+                navigationDelegate: (NavigationRequest request) {
+                  if (request.url.startsWith("https://www.thehabitgym.com/reflections/home/")) {
+                    homePageSaver(request.url);
+                  }
+                  if (!request.url.startsWith("https://www.thehabitgym.com/reflections/activate")) {
+                    setState(() {
+                      _visible = true;
+                    });
+                  }
+                  return NavigationDecision.navigate;
+                },
+              );
+            }
+        }
+      },
+    );
+  }
 }
-
-
-/*
- */
